372e23716868e57952b8dda2fb7f21a53bc2b9d7
sja47 <serge.arahal@gmail.com>
2024-09-22 10:31:37 +0300

    Update streamlit_app.py
    Exploring Tourism and Accommodation Trends in Selected Lebanese Towns

--- .github/workflows/streamlit_app.py
+++ .github/workflows/streamlit_app.py
@@ -1,3 +1,19 @@
+import pandas as pd
+from google.colab import files
+uploaded = files.upload()
+for filename in uploaded.keys():
+    df = pd.read_csv(filename)  # Load the CSV file
+    print(f"Loaded {filename} into DataFrame.")
+  print(df.head(50))
+pip install streamlit
+import streamlit as st
+import pandas as pd
+import numpy as np
+%%writefile app.py
+! wget -q -o - ipv4.icanhazip.com
+!apt-get install nodejs
+!npm install -g localtunnel@2.0.2
+! streamlit run app.py & npx localtunnel --port 8501
 import streamlit as st
 import pandas as pd
 import numpy as np
